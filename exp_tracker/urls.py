658bc94063f0d2d0996d6051ea2824a8b356c835
sai0566 <sreeram0566@gmail.com>
2026-04-28 15:38:37 +0530

    server main page2

--- exp_tracker/urls.py
+++ exp_tracker/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.http import HttpResponse
 from django.contrib import admin
-from django.urls import path,include,
+from django.urls import path,include
 def home(request):
     return HttpResponse('<h3>🚀server running successfully</h3>')
 
