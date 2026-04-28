a00e203d2f8cd086fb95d0421d5dacecbb0f0d79
sai0566 <sreeram0566@gmail.com>
2026-04-28 15:37:54 +0530

    server main page

--- exp_tracker/urls.py
+++ exp_tracker/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import HttpResponse
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,
+def home(request):
+    return HttpResponse('<h3>🚀server running successfully</h3>')
 
 urlpatterns = [
+    path('',home),
     path('admin/', admin.site.urls),
     path('api/',include('accounts.urls')),
     path('api/',include('expenses.urls'))
